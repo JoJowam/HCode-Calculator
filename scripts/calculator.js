55cd698b1c2d9ff45821cd4cca0b6abe04cd5560
Josué Villa Real <vilareal.on@gmail.com>
2023-10-27 12:13:06 -0300

    Add files via upload

---
+++ scripts/calculator.js
@@ -0,0 +1,5 @@
+//View - Interação com o usuário
+//Controller - Interação com o modelo e a view
+//Model - Trata os Dados
+// São 3 camadas de desenvolvimento isoladas
+window.calculator = new CalcController();
